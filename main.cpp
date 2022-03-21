d53c41098a6ff944d34c894755a67699395d7940
garthBates <garth.bates@wsu.edu>
2022-03-20 18:25:25 -0700

    Main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void welcome(void){
+    printf("Weclome to OARSMAN!\n");
+    printf("Add new entry: 1\n");
+}
+
+void menu(void){
+    int choice;
+    welcome();
+
+    /*
+    while(1){
+        choice = getchar();
+        printf("\n %d", choice);
+
+    }
+    */
+}
+
+int main()
+{
+    menu();
+    //welcome();
+    //printf("Welcome\n");
+}
